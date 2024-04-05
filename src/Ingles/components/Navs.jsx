73587c7a95fbad00e7d6bf2e56f56489517d0da9
JulioErives <julioerives345@gmail.com>
2024-04-04 20:39:35 -0700

    Prueba imagenes

--- src/Ingles/components/Navs.jsx
+++ src/Ingles/components/Navs.jsx
@@ -12,7 +12,7 @@ export function Navs() {
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
             <img
-              src="../assets/img/logo-1.png"
+              src="../public/img/logo-1.png"
               className="w-auto h-11"
               alt=""
             />
